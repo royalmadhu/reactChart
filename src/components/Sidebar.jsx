e6134ba3d72a3859af143fa32f054875b47e5486
madhukar <123929302+NitinSahu-DRL@users.noreply.github.com>
2024-05-19 14:42:06 +0530

    menu changes bg color set

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
+  // State to keep track of the selected item
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  // Function to handle click on an item
+  const handleItemClick = (index) => {
+    setSelectedItem(index);
+  };
+
   return (
     <div className="sidebar">
       <h2 className="text-2xl mt-16 ml-8 font-medium">Menu</h2>
       <ul className="ml-8 mt-4">
-        <li className="mb-2 font-normal">
-          <Link to="/">Dashboard</Link> 
+        <li
+          className={`mb-2 font-normal ${selectedItem === 0 ? 'bg-blue-500' : ''}`}
+          onClick={() => handleItemClick(0)}
+        >
+          <Link to="/">Dashboard</Link>
         </li>
-        <li className="mt-2">
-          <Link to="/dataSummary">Data Summary</Link> 
+        <li
+          className={`mt-2 ${selectedItem === 1 ? 'bg-blue-500' : ''}`}
+          onClick={() => handleItemClick(1)}
+        >
+          <Link to="/results">Data Summary</Link>
         </li>
-        <li className="mt-2">
-          <Link to="/results">Results</Link>
+        <li
+          className={`mt-2 ${selectedItem === 2 ? 'bg-blue-500' : ''}`}
+          onClick={() => handleItemClick(2)}
+        >
+          <Link to="/dataSummary">Results</Link>
         </li>
       </ul>
     </div>
