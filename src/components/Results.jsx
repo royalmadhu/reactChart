dc2aaa89ee82ffa1115d2fd5ec5b80acfbd992da
madhukar <123929302+NitinSahu-DRL@users.noreply.github.com>
2024-05-23 13:31:04 +0530

    code formatted

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function Skeleton() {
   return (
@@ -17,27 +24,24 @@ function Results({ data }) {
   if (!data || data.length === 0) {
     return <Skeleton />;
   }
-  let modifiedData = []; 
-   if(data && data.length > 0){
-    const minRPM = Math.min(...data?.map(item => item?.RPM));
-    const startingPoint = minRPM - (minRPM * 0.05);
+  let modifiedData = [];
+  if (data && data.length > 0) {
+    const minRPM = Math.min(...data?.map((item) => item?.RPM));
+    const startingPoint = minRPM - minRPM * 0.05;
 
+    const maxRPM = Math.max(...data?.map((item) => item?.RPM));
+    const endingPoint = maxRPM + maxRPM * 0.05;
 
-    const maxRPM = Math.max(...data?.map(item => item?.RPM));
-    const endingPoint = maxRPM + (maxRPM * 0.05);
-
-
-     modifiedData = data?.map(item => ({
+    modifiedData = data?.map((item) => ({
       ...item,
-      RPM: item?.RPM - startingPoint, 
-   }));
-   }
-    
+      RPM: item?.RPM - startingPoint,
+    }));
+  }
 
   return (
     <div className="container mx-auto w-1/2">
-      <div className='mt-32'>
-        <h2 className='ml-8 mb-4 text-2xl font-bold'>Time Vs RPM</h2>
+      <div className="mt-32">
+        <h2 className="ml-8 mb-4 text-2xl font-bold">Time Vs RPM</h2>
         <LineChart width={800} height={400} data={modifiedData}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" />
@@ -46,7 +50,7 @@ function Results({ data }) {
           <Legend />
           <Line type="monotone" dataKey="RPM" stroke="#8884d8" />
         </LineChart>
-        <h2 className='ml-8 mb-4 text-2xl font-bold'>Time Vs Torque (Nm)</h2>
+        <h2 className="ml-8 mb-4 text-2xl font-bold">Time Vs Torque (Nm)</h2>
         <LineChart width={800} height={400} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" />
@@ -55,7 +59,7 @@ function Results({ data }) {
           <Legend />
           <Line type="monotone" dataKey="Torque (Nm)" stroke="#8884d8" />
         </LineChart>
-        <h2 className='ml-8 mb-4 text-2xl font-bold'>Time Vs Hookload (kg)</h2>
+        <h2 className="ml-8 mb-4 text-2xl font-bold">Time Vs Hookload (kg)</h2>
         <LineChart width={800} height={400} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" />
@@ -64,7 +68,9 @@ function Results({ data }) {
           <Legend />
           <Line type="monotone" dataKey="Hookload (kg)" stroke="#8884d8" />
         </LineChart>
-        <h2 className='ml-8 mb-4 text-2xl font-bold'>Time Vs Mudflow (L/min)</h2>
+        <h2 className="ml-8 mb-4 text-2xl font-bold">
+          Time Vs Mudflow (L/min)
+        </h2>
         <LineChart width={800} height={400} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" />
@@ -73,7 +79,9 @@ function Results({ data }) {
           <Legend />
           <Line type="monotone" dataKey="Mudflow (L/min)" stroke="#8884d8" />
         </LineChart>
-        <h2 className='ml-8 mb-4 text-2xl font-bold'>Time Vs Mud Pressure (MPa)</h2>
+        <h2 className="ml-8 mb-4 text-2xl font-bold">
+          Time Vs Mud Pressure (MPa)
+        </h2>
         <LineChart width={800} height={400} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" />
@@ -83,7 +91,7 @@ function Results({ data }) {
           <Line type="monotone" dataKey="Mud Pressure (MPa)" stroke="#8884d8" />
         </LineChart>
       </div>
-      <div className='pb-16'></div>
+      <div className="pb-16"></div>
     </div>
   );
 }
